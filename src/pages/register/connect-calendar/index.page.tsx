0e6c893c553f0189879d95136ec9eb5b68c453ea
WesleyR10 <wesleyribas2015@gmail.com>
2024-02-22 15:40:22 -0300

    Creating availability page

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -6,7 +6,7 @@ import { ArrowRight, Check } from 'phosphor-react'
 import { Container, Header } from '../styles' // Estamos pegando do outro styles
 import { AuthError, ConnectBox, ConnectItem } from './styles'
 
-export default function Register() {
+export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter()
 
